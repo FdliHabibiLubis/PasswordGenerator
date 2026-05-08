4c72e911ad36282c70516a38ed8b83264a912c0c
Fdli Habibi Lubis <habibilubisf@gmail.com>
2026-05-08 10:06:26 +0700

    update

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 let passwordLength = document.getElementById("passwordLength");
 let password = document.getElementById("password");
+let saveButton = document.getElementById("saveButton");
 
 function generatePassword(length) {
-    const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
-    const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    const numeric = "123456789"
-    const symbol = "!@#$%^&*"
+  const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz";
+  const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numeric = "123456789";
+  const symbol = "!@#$%^&*";
 
-    const data  = lowerAlphabet + upperAlphabet + numeric + symbol;
-    let generator = "";
-    for(let index = 0; index < length; index++){
-        generator += data[~~(Math.random() * data.length)];
-    }
-    return generator;
+  const data = lowerAlphabet + upperAlphabet + numeric + symbol;
+  let generator = "";
+  for (let index = 0; index < length; index++) {
+    generator += data[~~(Math.random() * data.length)];
+  }
+  return generator;
 }
 
 function getPassword() {
-    const newPassword = generatePassword(passwordLength.value)
-    password.value = newPassword;
-    alert("Password has been generated");
+  const newPassword = generatePassword(passwordLength.value);
+  password.value = newPassword;
+  alert("Password has been generated");
 }
- 
-function savePassword() {
 
+function savePassword() {
+  document.title = password.value;
+  saveButton.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," 
+    +encodeURIComponent(`password saya : ${document.title}`),
+  );
+  saveButton.setAttribute('Download', 'MyPasswordGeneratorLOG.txt');
+  alert("Berhasil disimpan");
 }
